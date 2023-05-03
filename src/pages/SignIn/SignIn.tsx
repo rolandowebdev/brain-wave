d1c087e21327ce694cd3f2400b57ffeb20669719
RolandoPranataAR <rolandopranata@gmail.com>
2023-05-03 11:13:53 +0700

    chore: simplify parent element form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -9,7 +9,6 @@ import {
   HStack,
   Heading,
   Link,
-  Stack,
   Text,
   VStack,
 } from '@chakra-ui/react'
@@ -17,67 +16,71 @@ import { Link as RouterLink } from 'react-router-dom'
 
 export const SignIn = () => {
   return (
-    <Center>
-      <Stack spacing="4">
-        <VStack as="header" spacing="6" mt="8">
-          <GithubLogo />
-          <Heading as="h1" fontWeight="300" fontSize="24px">
-            Sign in to Brainwave
-          </Heading>
-        </VStack>
-        <Card
-          bg="brand.softDark"
-          color="brand.light"
-          variant="outline"
-          borderColor="brand.border"
-          w="308px">
-          <CardBody display="flex" gap={4} flexDir="column" as="form">
-            <FormControl as="fieldset">
-              <FormLabel fontSize="sm">Email address</FormLabel>
-              <CustomInput type="text" />
-            </FormControl>
-            <FormControl as="fieldset">
-              <HStack justify="space-between">
-                <FormLabel fontSize="sm">Password</FormLabel>
-                <Button
-                  as="a"
-                  href="#"
-                  variant="link"
-                  fontSize="xs"
-                  color="brand.primary"
-                  fontWeight="500">
-                  Forgot password?
-                </Button>
-              </HStack>
-              <CustomInput type="password" />
-            </FormControl>
-            <Button
-              bg="brand.secondary"
-              fontWeight="300"
-              size="sm"
-              _hover={{ bg: 'brand.secondary' }}
-              _active={{ bg: 'brand.secondary' }}>
-              Sign In
-            </Button>
-          </CardBody>
-        </Card>
-        <Card
-          variant="outline"
-          color="brand.light"
-          bgColor="brand.dark"
-          borderColor="brand.border">
-          <CardBody>
-            <Center>
-              <HStack fontSize="sm" spacing="1">
-                <Text>New to Brainwave?</Text>
-                <Link as={RouterLink} to="/signup" color="brand.primary">
-                  Create an account
-                </Link>
-              </HStack>
-            </Center>
-          </CardBody>
-        </Card>
-      </Stack>
+    <Center
+      as="section"
+      display="flex"
+      flexDir="column"
+      gap={4}
+      justifyContent="center"
+      alignItems="center">
+      <VStack as="header" spacing="6" mt="8">
+        <GithubLogo />
+        <Heading as="h1" fontWeight="300" fontSize="24px">
+          Sign in to Brainwave
+        </Heading>
+      </VStack>
+      <Card
+        bg="brand.softDark"
+        color="brand.light"
+        variant="outline"
+        borderColor="brand.border"
+        w="308px">
+        <CardBody display="flex" gap={4} flexDir="column" as="form">
+          <FormControl as="fieldset">
+            <FormLabel fontSize="sm">Email address</FormLabel>
+            <CustomInput type="text" />
+          </FormControl>
+          <FormControl as="fieldset">
+            <HStack justify="space-between">
+              <FormLabel fontSize="sm">Password</FormLabel>
+              <Button
+                as="a"
+                href="#"
+                variant="link"
+                fontSize="xs"
+                color="brand.primary"
+                fontWeight="500">
+                Forgot password?
+              </Button>
+            </HStack>
+            <CustomInput type="password" />
+          </FormControl>
+          <Button
+            bg="brand.secondary"
+            fontWeight="300"
+            size="sm"
+            _hover={{ bg: 'brand.secondary' }}
+            _active={{ bg: 'brand.secondary' }}>
+            Sign In
+          </Button>
+        </CardBody>
+      </Card>
+      <Card
+        variant="outline"
+        color="brand.light"
+        bgColor="brand.dark"
+        borderColor="brand.border">
+        <CardBody>
+          <Center>
+            <HStack fontSize="sm" spacing="1">
+              <Text>New to Brainwave?</Text>
+              <Link as={RouterLink} to="/signup" color="brand.primary">
+                Create an account
+              </Link>
+            </HStack>
+          </Center>
+        </CardBody>
+      </Card>
     </Center>
   )
 }
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -9,7 +9,6 @@ import {
   HStack,
   Heading,
   Link,
-  Stack,
   Text,
   VStack,
 } from '@chakra-ui/react'
@@ -17,60 +16,64 @@ import { Link as RouterLink } from 'react-router-dom'
 
 export const SignUp = () => {
   return (
-    <Center>
-      <Stack spacing="4">
-        <VStack as="header" spacing="6" mt="8">
-          <GithubLogo />
-          <Heading as="h1" fontWeight="300" fontSize="24px">
-            Sign up to Brainwave
-          </Heading>
-        </VStack>
-        <Card
-          bg="brand.softDark"
-          color="brand.light"
-          variant="outline"
-          borderColor="brand.border"
-          w="308px">
-          <CardBody display="flex" gap={4} flexDir="column" as="form">
-            <FormControl as="fieldset">
-              <FormLabel fontSize="sm">Email address</FormLabel>
-              <CustomInput type="text" />
-            </FormControl>
-            <FormControl as="fieldset">
-              <FormLabel fontSize="sm">Password</FormLabel>
-              <CustomInput type="password" />
-            </FormControl>
-            <FormControl as="fieldset">
-              <FormLabel fontSize="sm">Confirm Password</FormLabel>
-              <CustomInput type="password" />
-            </FormControl>
-            <Button
-              bg="brand.secondary"
-              fontWeight="300"
-              size="sm"
-              _hover={{ bg: 'brand.secondary' }}
-              _active={{ bg: 'brand.secondary' }}>
-              Sign Up
-            </Button>
-          </CardBody>
-        </Card>
-        <Card
-          variant="outline"
-          color="brand.light"
-          bgColor="brand.dark"
-          borderColor="brand.border">
-          <CardBody>
-            <Center>
-              <HStack fontSize="sm" spacing="1">
-                <Text>Already have an account?</Text>
-                <Link as={RouterLink} to="/signin" color="brand.primary">
-                  Sign In
-                </Link>
-              </HStack>
-            </Center>
-          </CardBody>
-        </Card>
-      </Stack>
+    <Center
+      as="section"
+      display="flex"
+      flexDir="column"
+      gap={4}
+      justifyContent="center"
+      alignItems="center">
+      <VStack as="header" spacing="6" mt="8">
+        <GithubLogo />
+        <Heading as="h1" fontWeight="300" fontSize="24px">
+          Sign up to Brainwave
+        </Heading>
+      </VStack>
+      <Card
+        bg="brand.softDark"
+        color="brand.light"
+        variant="outline"
+        borderColor="brand.border"
+        w="308px">
+        <CardBody display="flex" gap={4} flexDir="column" as="form">
+          <FormControl as="fieldset">
+            <FormLabel fontSize="sm">Email address</FormLabel>
+            <CustomInput type="text" />
+          </FormControl>
+          <FormControl as="fieldset">
+            <FormLabel fontSize="sm">Password</FormLabel>
+            <CustomInput type="password" />
+          </FormControl>
+          <FormControl as="fieldset">
+            <FormLabel fontSize="sm">Confirm Password</FormLabel>
+            <CustomInput type="password" />
+          </FormControl>
+          <Button
+            bg="brand.secondary"
+            fontWeight="300"
+            size="sm"
+            _hover={{ bg: 'brand.secondary' }}
+            _active={{ bg: 'brand.secondary' }}>
+            Sign Up
+          </Button>
+        </CardBody>
+      </Card>
+      <Card
+        variant="outline"
+        color="brand.light"
+        bgColor="brand.dark"
+        borderColor="brand.border">
+        <CardBody>
+          <Center>
+            <HStack fontSize="sm" spacing="1">
+              <Text>Already have an account?</Text>
+              <Link as={RouterLink} to="/signin" color="brand.primary">
+                Sign In
+              </Link>
+            </HStack>
+          </Center>
+        </CardBody>
+      </Card>
     </Center>
   )
 }
