99445f43514917cd4f23d468d17dfc154ee0d953
rolandowebdev <rolandopranata@gmail.com>
2023-11-04 19:49:12 +0700

    chore: remove hour from timer

--- src/utils/convertNumberToTimer.ts
+++ src/utils/convertNumberToTimer.ts
@@ -1,11 +1,10 @@
 export const convertNumberToTimer = (num: number): string => {
-  let hours: string | number = Math.floor(num / 3600)
+  const hours: string | number = Math.floor(num / 3600)
   let minutes: string | number = Math.floor((num - hours * 3600) / 60)
   let seconds: string | number = num - hours * 3600 - minutes * 60
 
-  hours = hours < 10 ? `0${hours}` : hours
   minutes = minutes < 10 ? `0${minutes}` : minutes
   seconds = seconds < 10 ? `0${seconds}` : seconds
 
-  return `${hours}:${minutes}:${seconds}`
+  return `${minutes}:${seconds}`
 }
